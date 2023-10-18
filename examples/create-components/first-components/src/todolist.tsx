508f8e67f66e6f382af2280aa304693cbdae0a66
tantran <duytantran.it@gmail.com>
2023-10-18 17:07:40 +0700

    Practice convert HTML to JSX and using with curly braces

---
+++ examples/create-components/first-components/src/todolist.tsx
@@ -0,0 +1,17 @@
+export function TodoList() {
+	return (
+		<>
+			<h1>ChiPu's Todos</h1>
+			<img
+				src='https://danviet.mediacdn.vn/296231569849192448/2022/7/18/3d45bf425133936dca22-1658133478447898181019.jpg'
+				alt='ChiPu'
+				className='todolist-image'
+			/>
+			<ul className='todo-list'>
+				<li>Invent new traffic lights</li>
+				<li>Rehearse a movie scene</li>
+				<li>Improve the spectrum technology</li>
+			</ul>
+		</>
+	);
+}
