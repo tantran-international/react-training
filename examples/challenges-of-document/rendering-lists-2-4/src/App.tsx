b5a944fc5d9813c1748327075ae6feec7499e879
Tan Tran <duytantran.it@gmail.com>
2023-10-20 11:11:38 +0700

    Update for challenge 3

--- examples/challenges-of-document/rendering-lists-2-4/src/App.tsx
+++ examples/challenges-of-document/rendering-lists-2-4/src/App.tsx
@@ -4,19 +4,31 @@
 import { recipes } from './data';
 import './App.css'
 
+// Define type of each property in object of recipes array
+type childrenType = {
+	name: string;
+	ingredients: string[]
+}
+
+function Recipe({name, ingredients}: childrenType) {
+	return (
+		<div>
+          <h2>{name}</h2>
+          <ul>
+            {ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+    </div>
+	);
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+				<Recipe {...recipe} key={recipe.id}/>
       ))}
     </div>
   );
