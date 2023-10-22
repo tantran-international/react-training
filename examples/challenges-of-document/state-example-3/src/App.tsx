5fb3f04d47471daa14078dbc232ccc9703066d48
tantran <duytantran.it@gmail.com>
2023-10-22 20:31:13 +0700

    Create use state challenge 3

--- examples/challenges-of-document/state-example-3/src/App.tsx
+++ examples/challenges-of-document/state-example-3/src/App.tsx
@@ -1,35 +1,31 @@
+// Challenge 3: When the feedback is submitted, it’s supposed to display a thank-you message.
+// However, it crashes with an error message saying “Rendered fewer hooks than expected”. Can you spot the mistake and fix it?
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function FeedbackForm() {
+	const [isSent, setIsSent] = useState(false);
+	const [message, setMessage] = useState<string>("");
+	if (isSent) {
+		return <h1>Thank you!</h1>;
+	} else {
+		// eslint-disable-next-line
+		return (
+			<form
+				onSubmit={(e: React.FormEvent) => {
+					e.preventDefault();
+					alert(`Sending: "${message}"`);
+					setIsSent(!isSent);
+				}}
+			>
+				<textarea
+					placeholder="Message"
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+				<br />
+				<button type="submit">Send</button>
+			</form>
+		);
+	}
 }
-
-export default App
