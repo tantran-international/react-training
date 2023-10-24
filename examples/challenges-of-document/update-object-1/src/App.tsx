7fc09cc825f7736df7fd0dbafb3f36d42fe7a95b
Tan Tran <duytantran.it@gmail.com>
2023-10-24 10:21:22 +0700

    Create update object challenge 1

--- examples/challenges-of-document/update-object-1/src/App.tsx
+++ examples/challenges-of-document/update-object-1/src/App.tsx
@@ -1,35 +1,46 @@
+// Challenge 1: Fix incorrect state updates
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 10
+  });
+
+  function handlePlusClick() {
+    setPlayer({ ...player, score: player.score + 1 });
+  }
+
+  function handleFirstNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value
+    });
+  }
+
+  function handleLastNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label>
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
+      </label>
+      <label>
+        First name:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:
+        <input value={player.lastName} onChange={handleLastNameChange} />
+      </label>
     </>
-  )
+  );
 }
-
-export default App
