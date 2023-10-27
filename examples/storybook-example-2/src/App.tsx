2043ec9c01c722028a9595de4792dd76184d2569
tantran <duytantran.it@gmail.com>
2023-10-27 10:01:31 +0700

    Create example components

--- examples/storybook-example-2/src/App.tsx
+++ examples/storybook-example-2/src/App.tsx
@@ -1,35 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [show, setShow] = useState(true);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <button style={{background: show ? "red" : "none"}}
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Show / Hide
+      </button>
+      {show ? <h2>Surprise!!!</h2> : null}
+    </div>
+  );
 }
-
-export default App
