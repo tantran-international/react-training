3001f0ff15a12458f462aa66bed9cfa3c83c4a95
tantran <duytantran.it@gmail.com.vn>
2023-11-14 15:09:26 +0700

    #76 update styles and basic layout

--- practice-one/src/App.tsx
+++ practice-one/src/App.tsx
@@ -1,18 +1,26 @@
 import { Popper } from '@components/commons/popper/popper';
 import { ListNavigation } from '@components/commons/list-navigation/list-navigation';
 import { Drawer } from '@components/parts/drawer/drawer';
+import { ToolBar } from '@components/parts/tool-bar/tool-bar';
 import './App.css';
 
 function App() {
   return (
     <>
-    <header className='user-manager-header'>User Manager</header>
-      <Drawer>
-        <div className='popper-wrapper'>
-          <Popper />
+      <header className='user-manager-header'>User Manager</header>
+
+      <div className='app-body'>
+        <Drawer>
+          <div className='popper-wrapper'>
+            <Popper />
+          </div>
+          <ListNavigation />
+        </Drawer>
+
+        <div className='app-content-wrapper'>
+          <ToolBar content='Users' />
         </div>
-        <ListNavigation />
-      </Drawer>
+      </div>
     </>
   );
 }
