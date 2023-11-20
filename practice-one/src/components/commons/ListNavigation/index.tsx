7f48c5060f5b0f5042a6c1e1731e5614c274bc2f
tantran <duytantran.it@gmail.com.vn>
2023-11-20 17:54:27 +0700

    #88 rename ListNavigation component and it's folder

--- practice-one/src/components/commons/ListNavigation/index.tsx
+++ practice-one/src/components/commons/ListNavigation/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 // Components
-import { ItemNavigation } from '../ItemNavigation';
+import { ItemNavigation } from '../ItemNavigation/index';
 
 // Icons
 import iconUserSelected from '@assets/images/icons/icon-user-selected.svg';
