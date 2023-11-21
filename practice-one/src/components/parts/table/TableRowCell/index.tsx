2fcfae3512e375aab4225a3ddc5b663039e0d9c9
tantran <duytantran.it@gmail.com.vn>
2023-11-21 17:44:50 +0700

    #92 refactoring and move types inside TableRowCell component

--- practice-one/src/components/parts/table/TableRowCell/index.tsx
+++ practice-one/src/components/parts/table/TableRowCell/index.tsx
@@ -1,12 +1,22 @@
-/* Types */
-import { ITableRowCellProps } from '@/types/ITableRowCell';
+/* Components */
+import { TableCell } from '@/components/parts/Table/TableCell';
+import { IColumnType } from '@/types/ITable';
+
+interface ITableRowCell<T> {
+  item: T;
+  column: IColumnType<T>;
+}
+
+const getObj = <T, Key extends keyof T>(obj: T, key: Key): T[Key] => {
+  return obj[key]
+}
 
 export const TableRowCell = <T,>({
   item,
   column
-}: ITableRowCellProps<T>): JSX.Element => {
-  const value = column['key'];
+}: ITableRowCell<T>): JSX.Element => {
+  const value = getObj(item, cpl;);
   return (
-    <>{column.render ? column.render(column, item) : value}</>
+    <TableCell>{column.render ? column.render(column, item) : value}</TableCell>
   );
 };
--- practice-one/src/types/ITableRowCell.ts
+++
@@ -1,7 +0,0 @@
-/* Types */
-import { IColumnType } from "@src/types/ITable";
-
-export interface ITableRowCellProps<T> {
-  item: T;
-  column: IColumnType<T>;
-}
