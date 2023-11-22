1db624779a88c60d3692716f022095d724de9926
tantran <duytantran.it@gmail.com.vn>
2023-11-22 23:07:32 +0700

    #92 optimize code for TableRow component

--- practice-one/src/components/parts/table/TableRow/index.tsx
+++ practice-one/src/components/parts/table/TableRow/index.tsx
@@ -4,14 +4,21 @@ import { ITableRow } from '@/types/ITableRow';
 /* Components */
 import { TableRowCell } from '@/components/parts/Table/TableRowCell/index';
 
-export const TableRow = <T,>({ data, columns }: ITableRow<T>): JSX.Element => {
-  console.log(data);
-  console.log(columns);
+export const TableRow = <T,>({
+  data,
+  columns
+}: ITableRow<T>): JSX.Element => {
   return (
     <>
-      {data.map((item, itemIndex) => (
+      {data.map((
+        item,
+        itemIndex
+        ) => (
         <tr key={`table-body-${itemIndex}`}>
-          {columns.map((column, columnIndex) => (
+          {columns.map((
+            column,
+            columnIndex
+            ) => (
             <TableRowCell
               key={`table-row-cell-${columnIndex}`}
               item={item}
