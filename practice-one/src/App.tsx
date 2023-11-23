a7bbc9a811f2efe6cafb8695bda64eb103f66c85
tantran <duytantran.it@gmail.com.vn>
2023-11-23 23:07:47 +0700

    #92 update paths and mock data for Table component in App component

--- practice-one/src/App.tsx
+++ practice-one/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import '@/App.css';
 
 /* Components */
 import { Popper } from '@/components/commons/Popper';
@@ -7,82 +7,203 @@ import { Drawer } from '@/components/parts/Drawer';
 import { ToolBar } from '@/components/parts/ToolBar';
 import { Table } from '@/components/parts/Table';
 import { Avatar } from '@/components/commons/Avatar';
+import { Status } from '@/components/commons/Status';
 
 /* Types */
 import { IColumnType } from '@/types/ITable';
-
 interface IData {
   avatar: string;
   fullName: string;
   status: boolean;
   email: string;
+  bgColor: string;
 }
 
+/* Define column's titles and datatypes as variable */
 const columns: IColumnType<IData>[] = [
   {
     key: 'avatar',
     title: '',
     render: (_, item) => (
-        <Avatar
-          additionalClass='avatar-row'
-          variant='rounded'
-          src={item.avatar != ''
-            ? item.avatar
-            : null
-          }
-          alt={item.fullName}/>
+      <Avatar
+        additionalClass='avatar-row'
+        variant='rounded'
+        src={item.avatar != '' ? item.avatar : null}
+        alt={item.fullName}
+        bgColor={item.bgColor}
+      />
     )
   },
   {
-    key: "fullName",
-    title: "Full Name",
+    key: 'fullName',
+    title: 'Full Name'
   },
   {
-    key: "status",
-    title: "Status"
+    key: 'status',
+    title: 'Status',
+    render: (_, item) => <Status isActive={item.status} />
   },
   {
-    key: "email",
-    title: "Email",
-  },
+    key: 'email',
+    title: 'Email'
+  }
 ];
 
+/* Mock data */
 const data: IData[] = [
   {
     avatar: '',
-    fullName: "Tran Duy Tan",
+    fullName: 'Tran Duy Tan',
     status: true,
-    email: "duytantran.it@gmail.com"
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
   },
   {
     avatar: '',
-    fullName: "Mua Hong",
+    fullName: 'Mua Hong',
     status: false,
-    email: "tranduytan597@gmail.com"
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
   },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  },
+  {
+    avatar: '',
+    fullName: 'Tran Duy Tan',
+    status: true,
+    email: 'duytantran.it@gmail.com',
+    bgColor: '#c79a1e'
+  },
+  {
+    avatar: '',
+    fullName: 'Mua Hong',
+    status: false,
+    email: 'tranduytan597@gmail.com',
+    bgColor: '#c71ec4'
+  }
 ];
 
 export const App = () => {
   return (
     <>
-      <header className='user-manager-header'>User Manager</header>
+      <header className='main-header'>User Manager</header>
 
-      <div className='app-body'>
+      <div className='main-body'>
+
         <Drawer>
-          <div className='popper-wrapper'>
-            <Popper />
-          </div>
+          <Popper />
           <ListNavigation />
         </Drawer>
 
         <div className='app-content-wrapper'>
           <ToolBar content='Users' />
-          <Table
-            data={data}
-            columns={columns}
-          />
+          <Table data={data} columns={columns} />
         </div>
+
       </div>
     </>
   );
-}
+};
