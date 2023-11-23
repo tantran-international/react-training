c2e4eb22e566f969efe3e760e199907c0e93bb35
tantran <duytantran.it@gmail.com.vn>
2023-11-23 23:01:54 +0700

    #92 replace types, update paths and refactoring TableHeader component

--- practice-one/src/components/parts/table/TableHeader/index.tsx
+++ practice-one/src/components/parts/table/TableHeader/index.tsx
@@ -1,16 +1,23 @@
+import '@/components/parts/Table/TableHeader/TableHeader.css';
+
 /* Components */
 import { TableHeaderCell } from '@/components/parts/Table/TableHeaderCell';
 
 /* Types */
-import { ITheader } from '@/types/ITableHeader';
+import { IColumnType } from '@/types/ITable';
+interface ITheader<T> {
+  columns: IColumnType<T>[];
+}
 
-export const TableHeader = <T,>({ columns }: ITheader<T>): JSX.Element => {
+export const TableHeader = <T,>({
+  columns
+}: ITheader<T>): JSX.Element => {
   return (
-    <tr>
+    <tr className="table-header">
       {columns.map((
         column,
         columnIndex
-        ) => (
+      ) => (
         <TableHeaderCell key={`table-head-cell-${columnIndex}`}>
           {column.title}
         </TableHeaderCell>
--- practice-one/src/types/ITableHeader.ts
+++
@@ -1,6 +0,0 @@
-/* Types */
-import { IColumnType } from "@/types/ITable";
-
-export interface ITheader<T> {
-  columns: IColumnType<T>[];
-}
