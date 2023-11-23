fbb944d07c4517f4251c0bb639f21eb9626a99b1
tantran <duytantran.it@gmail.com.vn>
2023-11-23 23:03:34 +0700

    #92 update paths and refactoring TableRow component

--- practice-one/src/components/parts/table/TableRow/index.tsx
+++ practice-one/src/components/parts/table/TableRow/index.tsx
@@ -1,23 +1,40 @@
 /* Types */
-import { ITableRow } from '@/types/ITableRow';
+import { IColumnType } from "@/types/ITable";
+interface ITableRow<T> {
+  data: T[];
+  columns: IColumnType<T>[]
+}
 
 /* Components */
-import { TableRowCell } from '@/components/parts/Table/TableRowCell/index';
+import { TableRowItem } from '@/components/parts/Table/TableRowItem';
+import { TableRowCell } from '@/components/parts/Table/TableRowCell';
+import { useState } from 'react';
 
-export const TableRow = <T,>({
-  data,
-  columns
-}: ITableRow<T>): JSX.Element => {
+export const TableRow = <T,>({ data, columns }: ITableRow<T>): JSX.Element => {
+  /* Initial "0" don't match with any index of TableRowItems == none TableRowItem is selected */
+  const [selected, setSelected] = useState(0);
+
+  /**
+   *
+   * @param index - index of Object is rendered as TableRowItem in (Array "data")
+   */
+  const handleSelectedRow = (index: number) => {
+    setSelected(index);
+  };
+
   return (
     <>
-      {data.map((
-        item,
-        itemIndex
-        ) => (
-        <tr key={`table-body-${itemIndex}`}>
+      {data.map((item, itemIndex) => (
+        <TableRowItem
+          key={`table-body-${itemIndex}`}
+          /* (param + 1) to avoid initial value of state */
+          index={itemIndex + 1}
+          selected={selected}
+          onRowClick={handleSelectedRow}
+        >
           {columns.map((
-            column,
-            columnIndex
+              column,
+              columnIndex
             ) => (
             <TableRowCell
               key={`table-row-cell-${columnIndex}`}
@@ -25,7 +42,7 @@ export const TableRow = <T,>({
               column={column}
             />
           ))}
-        </tr>
+        </TableRowItem>
       ))}
     </>
   );
--- practice-one/src/types/ITableRow.ts
+++
@@ -1,6 +0,0 @@
-import { IColumnType } from "@/types/ITable";
-
-export interface ITableRow<T> {
-  data: T[];
-  columns: IColumnType<T>[]
-}
