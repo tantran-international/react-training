743741a6f9fae91b036ce5895b74bb31b2f7b704
tantran <duytantran.it@gmail.com.vn>
2023-11-24 10:05:26 +0700

    #97 update for render confirm Modal, refactoring and update styles for Modal component

--- practice-one/src/components/commons/modal/index.tsx
+++ practice-one/src/components/commons/modal/index.tsx
@@ -5,11 +5,21 @@ import '@/components/commons/Modal/Modal.css';
 import { Button } from '@/components/commons/Button';
 
 // Types
-import { TModal } from '@/types/TModal';
+interface IModal {
+  onClose?: () => void;
+  modalTitle?: string;
+  modalDescription?: string;
+  additionalClass?: string;
+  isOpen: boolean;
+  btnTextPrimary?: string;
+  btnTextSecondary?: string;
+  handleClickBtnPrimary?: () => void;
+  handleClickBtnSecondary?: () => void;
+  children?: JSX.Element[] | JSX.Element;
+}
 
 // Icons
 import iconClose from '@/assets/images/icons/icon-close.svg';
-
 
 export function Modal({
   onClose,
@@ -18,40 +28,72 @@ export function Modal({
   additionalClass,
   isOpen,
   btnTextPrimary,
-  onClickBtnPrimary,
+  btnTextSecondary,
+  handleClickBtnPrimary,
+  handleClickBtnSecondary,
   children
-}: TModal) {
+}: IModal) {
   return (
     <>
       {isOpen &&
         createPortal(
-          <div className='modal-wrapper'>
-            <div className={`modal modal-${additionalClass}`}>
-              <div className={`modal-${additionalClass}-header`}>
-                {modalTitle && <h2 className='modal-title'>{modalTitle}</h2>}
-                {modalDescription && (
-                  <p className='modal-description'>{modalDescription}</p>
-                )}
-                <Button
-                  icon={iconClose}
-                  additionalClass='close'
-                  onClick={onClose}
-                />
+          <div
+            className={`modal-wrapper ${additionalClass}-wrapper`}
+            onClick={onClose}
+          >
+            <div
+              className={`modal ${additionalClass}`}
+              onClick={(event) => event.stopPropagation()}
+            >
+              <div className={`${additionalClass}-header`}>
+                {modalTitle
+                ? (
+                    <>
+                      <h2 className="modal-title">{modalTitle}</h2>
+                      <p className="modal-description">{modalDescription}</p>
+                    </>
+                  )
+                : (
+                    <>
+                      <p className="modal-description">{modalDescription}</p>
+                      <Button
+                        icon={iconClose}
+                        additionalClass="button-icon-close"
+                        onClick={onClose}
+                      />
+                    </>
+                  )}
               </div>
-              <div className={`modal-${additionalClass}-body`}>
-                {children}
-                {btnTextPrimary && (
-                  <Button
-                    content={btnTextPrimary}
-                    additionalClass='primary button-save'
-                    onClick={onClickBtnPrimary}
-                  />
-                )}
+
+              <div className={`${additionalClass}-body`}>
+                {modalTitle
+                ? (
+                    <>
+                      <Button
+                        content={btnTextSecondary}
+                        additionalClass="button-secondary button-save"
+                        onClick={onClose}
+                      />
+                      <Button
+                        content={btnTextPrimary}
+                        additionalClass="button-primary button-save"
+                        onClick={handleClickBtnPrimary}
+                      />
+                    </>
+                  )
+                : (
+                    <>
+                      {children}
+                      <Button
+                        content={btnTextPrimary}
+                        additionalClass="button-primary button-save"
+                        onClick={handleClickBtnPrimary}
+                      />
+                    </>
+                  )}
               </div>
             </div>
-          </div>,
-          document.body
-        )}
+          </div>, document.body )}
     </>
   );
 }
