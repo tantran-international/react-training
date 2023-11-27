0fb9315e5c17f31d6364b2a82419d510c13f2aaa
tantran <duytantran.it@gmail.com.vn>
2023-11-27 17:04:10 +0700

    #105 create TabButton component

---
+++ practice-one/src/components/parts/Tab/TabButton/index.tsx
@@ -0,0 +1,29 @@
+/* Types */
+interface ITabButton {
+  title: string;
+  isActiveTab: boolean;
+  index: number;
+  onTabClick: (index: number) => void;
+}
+export const TabButton = ({
+  title,
+  isActiveTab,
+  index,
+  onTabClick,
+}: ITabButton) => {
+  const handleTabClick = () => {
+    onTabClick(index);
+  };
+  return (
+    <button
+      className={`tab-item ${isActiveTab
+        ? 'tab-item-active'
+        : ''
+      }`}
+      type='button'
+      onClick={handleTabClick}
+    >
+      {title}
+    </button>
+  );
+};
