867e4e6e2767d1d89e80ed4734391a24182a5c2c
tantran <duytantran.it@gmail.com.vn>
2023-11-27 22:06:46 +0700

    #111 create interface for data

---
+++ practice-one/src/types/IData.ts
@@ -0,0 +1,10 @@
+export interface IData {
+  avatar: string;
+  fullName: string;
+  email: string;
+  status: boolean;
+  registered: string;
+  lastVisitedDate: string;
+  details: string;
+  bgColor: string;
+}
