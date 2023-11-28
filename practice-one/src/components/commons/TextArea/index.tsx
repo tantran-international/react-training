35731948e6a592a4709370cf6ea26db367633a6a
TanTran <duytantran.it@gmail.com>
2023-11-28 17:10:58 +0700

    #125 update TextArea component's ui

--- practice-one/src/components/commons/TextArea/index.tsx
+++ practice-one/src/components/commons/TextArea/index.tsx
@@ -1,22 +1,34 @@
+import { FormEvent } from 'react';
 import './TextArea.css';
 
 /* Types */
 type TTextArea = {
-  additionalClass: string;
-  onChange: () => void;
-  value: string;
+  additionalClass?: string;
+  onDetailsChange: (value: string) => void;
+  value?: string;
 };
 
-export const TextArea = ({ additionalClass, onChange, value }: TTextArea) => {
+export const TextArea = ({
+  additionalClass,
+  onDetailsChange,
+  value
+}: TTextArea) => {
+
+  /**
+   * Update fullName state of TextField when it's changed
+   * @param event - event onChane of input
+   */
+  const handleDetailsChange = (event: FormEvent<HTMLTextAreaElement>) => {
+    onDetailsChange?.(event.currentTarget.value);
+  };
 
   return (
     <textarea
       id='userDetail'
       name='userDetail'
       className={`text-area ${additionalClass}`}
-      onChange={onChange}
+      onChange={handleDetailsChange}
       value={value}
-      autoFocus
     ></textarea>
   );
 };
